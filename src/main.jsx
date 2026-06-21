fbbf18870824b1c7cb78666fe4e0f566285d2784
elipauly <elpauly@uiowa.edu>
2026-06-21 15:46:59 +0200

    added column layout and navbar structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import App from './App.jsx'
+import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
