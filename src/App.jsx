90cc68f2c807dee13742c68f52d748a83a9c9856
elipauly <elpauly@uiowa.edu>
2026-08-11 18:08:05 -0500

    file import naming mistype

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import JumpToTop from './components/JumpToTop.jsx';
 //pages
 import Home from './Home.jsx'
 import About from './About.jsx'
-import Research from './research.jsx' //dont know why this needs to be lowercase, but it works either way
+import Research from './Research.jsx' //dont know why this needs to be lowercase, but it works either way
 import SportsBettingReddit from './researchpages/SportsBettingReddit.jsx';
 import CommercializationMMO from './researchpages/CommercializationMMO.jsx';
 
